21c6f0e0a2f82fad515c2a77bb5ab6607bb468fc
daniela <d.ortizkosobucki@gmail.com>
2022-07-21 10:50:03 -0400

    no undefined message, array of objects

--- trivia/src/components/Questions.js
+++ trivia/src/components/Questions.js
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function Questions({ categoryURL }) {
-    const [question, setQuestion] = useState()
+    const [question, setQuestion] = useState([])
+    function decodeHtml(html) {
+        let txt = document.createElement("textarea");
+        txt.innerHTML = html;
+        return txt.value;
+    }
     useEffect(() => {
         console.log(`url: ${categoryURL}`)
         axios
             .get(categoryURL)
-            .then((res) => setQuestion(res.data.results[0].question))
-    }, [])
+            .then((res) => setQuestion(res.data.results))
+    }, [categoryURL])
     return (
         <div>
             <h1>Question 1</h1>
-            <p>{question}</p>
+            <p>{decodeHtml(question)}</p>
         </div>
     )
 }
